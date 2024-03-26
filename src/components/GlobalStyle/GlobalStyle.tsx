cc020258fae887907a7ab76dfc0ae99e1e0f14f8
nasiri <futinasiri7@gmail.com>
2024-03-26 14:09:46 +0330

    first commit

---
+++ src/components/GlobalStyle/GlobalStyle.tsx
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize};
+
+  body {
+    font-size: 18px;
+    /** Disable eslastic scrolling on mobile */
+    overflow: hidden;
+    overscroll-behavior: none;
+  }
+
+  #game {
+    width: 100vw;
+    height: 100vh;
+  }
+`;
+
+export default GlobalStyle;
